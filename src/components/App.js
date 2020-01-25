4170f4ba0cde56cfecbe79f6547c20465dadfa8f
filipe custodio <filipe.l.custodio@gmail.com>
2020-01-25 21:43:04 +0000

    moving apps dispatch call to parent

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,14 @@ import SearchBar from "./SearchBar";
 import SideBarList from "./SideBarList";
 import AppList from "./AppList";
 import Pagination from "./Pagination";
+import { connect } from "react-redux";
+import { fetchApps } from "../actions";
 
 class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchApps();
+  }
+
   render() {
     return (
       <div className="flex-container">
@@ -21,4 +27,11 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return { apps: state.apps };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchApps }
+)(App);
