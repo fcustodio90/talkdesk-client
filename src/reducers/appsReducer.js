3f18bd54e9472bf993a3886f16b794346e223606
filipe custodio <filipe.l.custodio@gmail.com>
2020-01-25 21:31:59 +0000

    Removing selected category from react state to redux store

--- src/reducers/appsReducer.js
+++ src/reducers/appsReducer.js
@@ -1,10 +1,10 @@
-import { FETCH_APPS } from "../actions/types";
+import { FETCH_APPS, FETCH_APPS_BY_CATEGORY } from "../actions/types";
 
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_APPS:
       return action.payload;
-    case "FETCH_APPS_BY_CATEGORY":
+    case FETCH_APPS_BY_CATEGORY:
       return action.payload;
     default:
       return state;
